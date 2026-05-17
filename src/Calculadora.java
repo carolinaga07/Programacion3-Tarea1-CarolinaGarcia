db858837068f4a369cdfe967cbcc820cd65b9b3f
carolina <carolina@MONNIE>
2026-05-17 00:55:24 -0400

    creacion de los metodos operacionales + edicion del menu.

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -6,6 +6,7 @@ public class Calculadora {
     static void main (String [] args){
 
         Scanner sc = new Scanner(System.in);
+        
 
         System.out.println("Introduce el primer numero:");
         if (sc.hasNextDouble()){
@@ -23,7 +24,55 @@ public class Calculadora {
             System.out.println("Entrada invalida, debe ser un numero");
         }
 
-        
+
+        do{
+            MostrarMenuCalculadora();
+            int opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    Calculadora.Sumar( );
+                    break;
+                case 2:
+                    Calculadora.Restar();
+                    break;
+                case 3:
+                    Calculadora.Multiplicar();
+                    break;
+                case 4:
+                    Calculadora.Dividir();
+            
+              
+            }
+        }
+
+    }
+
+
+    public double Sumar( double numero1, double numero2){
+
+       return numero1 + numero2;
+
+    }
+
+    public double Restar( double numero1, double numero2){
+
+       return numero1 - numero2;
+       
+
+    }
+
+    public double Multiplicar( double numero1, double numero2){
+
+       return  numero1 * numero2;
+      
+
+    }
+
+    public double Dividir( double numero1, double numero2){
+
+       return numero1 / numero2;
+       
+
     }
     
 }
