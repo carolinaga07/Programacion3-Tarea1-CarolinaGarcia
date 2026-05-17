ed954543e05d02520af7949cdabbc1d01c7ce056
carolina <carolina@MONNIE>
2026-05-17 00:32:43 -0400

    primeros pasos de la clase calculadora

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,3 +1,29 @@
+
+import java.util.Scanner;
+
 public class Calculadora {
+
+    static void main (String [] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Introduce el primer numero:");
+        if (sc.hasNextDouble()){
+            double numero1 = sc.nextDouble();
+        }
+        else{
+            System.out.println("Entrada invalida, debe ser un numero");
+        }
+
+        System.out.println("Introduce el segundo numero:");
+        if(sc.hasNextDouble()){
+            double numero2 = sc.nextDouble();
+        }
+        else{
+            System.out.println("Entrada invalida, debe ser un numero");
+        }
+
+        
+    }
     
 }
