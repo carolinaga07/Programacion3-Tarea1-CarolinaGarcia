cccaa4715dd53c9dedf3e900f843f56810a4b13d
carolina <carolina@MONNIE>
2026-05-17 02:36:18 -0400

    finalizacion de clase tabla de multiplicar

--- src/TablaMultiplicar.java
+++ src/TablaMultiplicar.java
@@ -1,3 +1,30 @@
+import java.util.Scanner;
+
 public class TablaMultiplicar {
-    System.out.println("ingresa un numero entero ")
+   
+    public static void main (String [] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("ingrese un numero para conocer su tabla de multiplicar");
+        int num = sc.nextInt();
+
+        MostrarTablaMultiplicar(num);
+        
+       
+    } 
+    public static void MostrarTablaMultiplicar(int num){
+        
+         int contador = 1;
+         System.out.println("========================");
+         System.out.println("TABLA DE MULTIPLICAR DEL"  +  num);
+         System.out.println("========================");
+         
+         while (contador <= 10)
+        {
+            int multiplicar = num * contador;
+            System.out.println(num + "x" + contador + "="+ multiplicar  );
+            contador++;
+        }
+    }
 }
