f560aea3ed6df373e0ba0015d42ba326d4c8711d
carolina <carolina@MONNIE>
2026-05-17 13:25:06 -0400

    creacion del metodo cuenta bancaria

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -1,3 +1,21 @@
+
+import java.util.Scanner;
+
 public class CuentaBancaria {
+
+    private int NumeroDeCuenta;
+    private String Titular;
+    private String TipoDeCuenta;
+    private int Saldo;
+
+    public CuentaBancaria( int numerodecuenta, String titular, String tipodecuenta, int saldo){
+        this.TipoDeCuenta = tipodecuenta;
+        this.Titular = titular;
+        this.NumeroDeCuenta = numerodecuenta;
+        this.Saldo = saldo;
+    }
+
+    
+
     
 }
