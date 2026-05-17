bad0c442b2229cf0c2e0e16a56d8870c21207fc9
carolina <carolina@MONNIE>
2026-05-17 03:21:05 -0400

    finalizacion de la funcion conteo de vocales

--- src/ContadorVocales.java
+++ src/ContadorVocales.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class ContadorVocales {
+    
+    public static void ContarVocales(){
+       
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Ingrese una frase:");
+        String cadena = sc.nextLine();
+        
+        int Vocales = 0;
+
+        for(int i = 0; i < cadena.length(); i++){
+            
+            char caracter = Character.toLowerCase(cadena.charAt(i));
+            
+            if (caracter == 'a'|| caracter == 'e' || caracter == 'i' || caracter == 'o' || caracter == 'u'){
+                Vocales++;
+            }
+             
+            
+        }
+
+        System.out.println("Cantidad de vocales:" + Vocales);
+    }
+    
+
+   
+
+    
     
 }
