3e5800bd7e443e20725391aacde5a5ad663acbc1
carolina <carolina@MONNIE>
2026-05-17 17:47:10 -0400

    clase inventario terminada

--- src/Inventario.java
+++ src/Inventario.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Inventario {
     
-    public static void main(Scanner sc){
+    public static void Ejecutar (Scanner sc){
         ArrayList<Productos> lista = new ArrayList<>();
         int opcion;
 
@@ -13,8 +13,7 @@ public class Inventario {
         System.out.println("======================");
         System.out.println( "  1. Agregar producto");
         System.out.println( "  2. Listar productos");
-        System.out.println("   3.Buscar producto  ");
-        System.out.println( "  4. Volver al menu  ");
+        System.out.println( "  3. Volver al menu  ");
         System.out.println("======================");
          System.out.println( "Seleccione una opcion:");
          opcion = sc.nextInt();
@@ -27,13 +26,23 @@ public class Inventario {
                 int cantidad = sc.nextInt();
                 System.out.println("Precio:");
                 double precio = sc.nextDouble();
-                                
+                lista.add(new Productos(nombre, cantidad, precio));
+                System.out.println("Producto Agregado");                 
                 break;
-         
-            default:
+            case 2:
+                if(lista.isEmpty()){
+                    System.out.println("no hay productos"); 
+                }
+                else{
+                    for(int i = 0; i < lista.size(); i++){
+                        Productos p = lista.get(i);
+                        System.out.println("." + p.getNombre() + "|" + p.getCantidad() + "|" + p.getPrecio()); 
+                    }
+                }
                 break;
+
          }
 
-        }
+        }while (opcion != 3);
     }
 }
