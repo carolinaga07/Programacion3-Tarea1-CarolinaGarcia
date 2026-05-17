04c273e1ae6a9c66e1094d9704678cfc5851fc16
carolina <carolina@MONNIE>
2026-05-17 13:38:20 -0400

    aplicacion de getters y setters + creeacion de metodos para agregar o disminuir el saldo

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -7,6 +7,7 @@ public class CuentaBancaria {
     private String Titular;
     private String TipoDeCuenta;
     private int Saldo;
+    
 
     public CuentaBancaria( int numerodecuenta, String titular, String tipodecuenta, int saldo){
         this.TipoDeCuenta = tipodecuenta;
@@ -15,7 +16,51 @@ public class CuentaBancaria {
         this.Saldo = saldo;
     }
 
-    
+    public String getTipoDeCuenta(){
+        return TipoDeCuenta;
+    }
+
+    public void setTipoDeCuenta(String TipoDeCuenta){
+        this.TipoDeCuenta = TipoDeCuenta;
+    }
+
+    public String getTitular(){
+        return Titular;
+    }
+
+    public void setTitular(String Titular){
+        this.Titular = Titular;
+    }
+
+    public int getNumeroDeCuenta(){
+        return NumeroDeCuenta;
+    }
+
+    public void setNumeroDeCuenta(int NumeroDeCuenta){
+        this.NumeroDeCuenta = NumeroDeCuenta;
+    }
+
+    public int getSaldo(){
+        return Saldo;
+    }
+
+    public void setSaldo(int Saldo){
+        this.Saldo = Saldo;
+    }
+
+    public int Retiro( int CantidadRetirar){
+
+        int resultado = Saldo - CantidadRetirar;
+        return resultado;
+
+    }
+
+    public int Deposito ( int CantidadDepositar){
+
+        int resultado = Saldo - CantidadDepositar;
+        return resultado;
+
+    }
 
     
 }
