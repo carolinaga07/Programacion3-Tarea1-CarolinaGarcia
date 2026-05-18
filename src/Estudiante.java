1872e0280fa378569953e80db4ad004e1bc17718
carolina <carolina@MONNIE>
2026-05-18 10:19:14 -0400

    cambios en la clase estudiante para su funcionamiento

--- src/Estudiante.java
+++ src/Estudiante.java
@@ -60,9 +60,9 @@ public class Estudiante {
            System.out.println(    "Matricula: " + getMatricula());
            System.out.println(    "Carrera: " + getCarrera());
            System.out.println("======================");
-           System.out.println(    Nombre + Apellido + 
-            "con matricula" + Matricula + 
-            "es estudiante de " + Carrera);
+           System.out.println(this.Nombre + " "+ this.Apellido + 
+            " con matricula" + this.Matricula + 
+            " es estudiante de " + this.Carrera);
    }
     
 }
